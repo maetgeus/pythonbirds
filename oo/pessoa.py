d04b4fa1402aa5527a18b7f0559525fb38ad800a
maetgeus <Ma*62182561>
2021-06-10 22:06:27 -0300

    Criados atributos de dados de nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,12 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=None):
+        self.idade = idade
+        self.nome = nome
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
